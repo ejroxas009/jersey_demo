f677e6c3d0dc4e01e4c2373f6081735c614f941d
GroupFour77 <sevenseven403@yahoo.com>
2022-10-28 20:12:54 +0800

    CRUD except update

--- src/main/java/org/revalida/example/spring_jersey_demo/controller/AccountController.java
+++ src/main/java/org/revalida/example/spring_jersey_demo/controller/AccountController.java
@@ -1,12 +1,13 @@
 package org.revalida.example.spring_jersey_demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -53,6 +54,27 @@ public class AccountController {
 	@Consumes({MediaType.APPLICATION_JSON})
 	public Response saveAccount(Account account) {
 		accountRepo.save(account);
+		return Response.ok().build();
+	}
+	
+//	@PUT
+//	@Path("/{accountId}")
+//	@Produces({MediaType.APPLICATION_JSON})
+//	@Consumes({MediaType.APPLICATION_JSON})
+//	public Response editAccount(@PathParam("accountId") Long id ,Account account) {
+//		
+//		
+//	}
+	@DELETE
+	@Path("/{accountId}")
+	public Response deleteAccountById(@PathParam("accountId") Long id) {
+		accountRepo.deleteById(id);
+		return Response.ok().build();
+	}
+	
+	@DELETE
+	public Response deleteAllAccount() {
+		accountRepo.deleteAll();
 		return Response.ok().build();
 	}
 }
